7277e76a28927fc83f0cccbc6805fcc17c0d2ab2
Rebecca Crecelius <info@rebeccacrecelius.com>
2024-05-01 20:48:10 +0000

    Update hamburger menu for mobile naviagation

--- scripts/main.js
+++ scripts/main.js
@@ -10,3 +10,13 @@ function runOnScroll(){
     $('html,body').animate({scrollTop: 0},1000);
     window.removeEventListener("scroll", runOnScroll);
 }
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".main-nav");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
