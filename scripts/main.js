a97f3cfefbfb7da303c619b9fa87d830aad944c8
Rebecca Crecelius <info@rebeccacrecelius.com>
2024-06-23 22:36:28 +0000

    Add js file

--- scripts/main.js
+++ scripts/main.js
@@ -10,13 +10,3 @@ function runOnScroll() {
     $('html,body').animate({ scrollTop: 0 }, 1000);
     window.removeEventListener("scroll", runOnScroll);
 }
-
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".main-nav");
-
-hamburger.addEventListener("click", mobileMenu);
-
-function mobileMenu() {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-}
