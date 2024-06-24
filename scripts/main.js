136d2997f1d7678fde8c4391de19bc7d500eab34
Rebecca Crecelius <info@rebeccacrecelius.com>
2024-06-24 16:41:20 +0000

    Update hamburger menu on index.html

--- scripts/main.js
+++ scripts/main.js
@@ -10,3 +10,8 @@ function runOnScroll() {
     $('html,body').animate({ scrollTop: 0 }, 1000);
     window.removeEventListener("scroll", runOnScroll);
 }
+
+document.querySelector('.hamburger-button').addEventListener('click', function() {
+    document.querySelector('.nav-links').style.display = 
+    (document.querySelector('.nav-links').style.display == 'none') ? 'block' : 'none';
+ });
