7d948fd2996419eb20ef37a99cda1d7d59fb15e2
Rebecca Crecelius <info@rebeccacrecelius.com>
2024-07-10 01:39:01 +0000

    Add screenshots for websites#

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,17 @@
 $(document).ready(function () {
+    window.addEventListener("scroll", runOnScroll);
+
+    function runOnScroll() {
+        $('html,body').animate({ scrollTop: 0 }, 1000);
+        window.removeEventListener("scroll", runOnScroll);
+    };
+/*
     $('.hotspot').click(function () {
         $(this).toggleClass('clicked');
     });
+*/
 });
 
-window.addEventListener("scroll", runOnScroll);
-
-function runOnScroll() {
-    $('html,body').animate({ scrollTop: 0 }, 1000);
-    window.removeEventListener("scroll", runOnScroll);
-}
 
 document.querySelector('.hamburger-button').addEventListener('click', function() {
     document.querySelector('.nav-links').style.display = 
